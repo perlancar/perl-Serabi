4ec25b0a329e5e80b4506f124a7665d66fad72f4
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-31 23:11:28 +0700

    implement about command

--- lib/Sub/Spec/HTTP/Server/Command/about.pm
+++ lib/Sub/Spec/HTTP/Server/Command/about.pm
@@ -1,51 +1,35 @@
-package Plack::Middleware::SubSpec::Command::listmod;
+package Sub::Spec::HTTP::Server::Command;
 
 use 5.010;
 use strict;
 use warnings;
 
-use parent qw(Plack::Middleware);
-#use Plack::Util::Accessor qw();
-
-use Plack::Util::SubSpec qw(errpage);
-
 # VERSION
 
-sub prepare_app {
-    my $self = shift;
-    die "Not yet implemented";
-}
+sub handle_about {
+    my ($env) = @_;
+    my $ssreq = $env->{"ss.request"};
 
-sub call {
-    my ($self, $env) = @_;
-
-    # continue to app
-    $self->app->($env);
+    my $ssu = $ssreq->{uri};
+    return [200, "OK", {
+        uri     => ($ssu ? $ssu->{_uri} : undef),
+        module  => ($ssu ? $ssu->module : undef),
+        sub     => ($ssu ? $ssu->sub    : undef),
+        args    => ($ssu ? $ssu->args   : undef),
+    }];
 }
 
 1;
-# ABSTRACT: List available modules
-__END__
+# ABSTRACT: Return information about the server and request
 
 =head1 SYNOPSIS
 
- # In app.psgi
- use Plack::Builder;
-
- builder {
-    enable "SubSpec::Command::listmod";
- };
+ # used by Plack::Middleware::SubSpec::HandleCommand
 
 
 =head1 DESCRIPTION
 
-This middleware executes 'listmod' command.
-
-
-=head1 CONFIGURATION
-
-=over 4
-
-=back
+This command returns a hashref containing information about the server and
+request, e.g.: 'module', 'sub', 'args'
 
 =cut
